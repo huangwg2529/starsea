e2bff3d35adada582f61c187a781aabeebd806ae
huangwg2529 <hwg2529@163.com>
2019-08-26 12:50:45 +0800

    first demo

--- src/main/java/com/starsea/controller/LoginController.java
+++ src/main/java/com/starsea/controller/LoginController.java
@@ -25,18 +25,13 @@ public class LoginController {
     @Autowired
     private BookDao bookDao;
 
-    /**
-     * 登录控制器，前后端分离用的不同协议和端口，所以需要加入@CrossOrigin支持跨域。
-     * 给VueLoginInfoVo对象加入@Valid注解，并在参数中加入BindingResult来获取错误信息。
-     * 在逻辑处理中我们判断BindingResult是否含有错误信息，如果有错误信息，则直接返回错误信息。
-     */
     @CrossOrigin
     @RequestMapping(value = "/api/login", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
     @ResponseBody
     public Result login(@Valid @RequestBody VueLoginInfoVo loginInfoVo, BindingResult bindingResult) {
         //验证输入
         if (bindingResult.hasErrors()) {
-            String message = String.format("登录失败，详细信息[%s]。", bindingResult.getFieldError().getDefaultMessage());
+            String message = String.format("登录失败:%s。", bindingResult.getFieldError().getDefaultMessage());
             return ResultFactory.buildFailResult(message);
         }
 
@@ -67,7 +62,7 @@ public class LoginController {
         System.out.println("register");
         //验证输入
         if (bindingResult.hasErrors()) {
-            String message = String.format("注册失败，详细信息[%s]。", bindingResult.getFieldError().getDefaultMessage());
+            String message = String.format("注册失败: %s。", bindingResult.getFieldError().getDefaultMessage());
             return ResultFactory.buildFailResult(message);
         }
 
--- src/main/java/com/starsea/controller/showBookController.java
+++ src/main/java/com/starsea/controller/showBookController.java
@@ -11,37 +11,13 @@ public class showBookController {
     @Autowired
     private BookDao bookDao;
 
-
     @CrossOrigin
-    //@RequestMapping(value = "/api/index", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
-    //@ResponseBody
-    @RequestMapping(value = "/api/index", method = RequestMethod.GET)
-    @ResponseBody
-    public String showBook(@RequestParam("name") String name){
-        System.out.println(name);
-        return name + " get and post!";
-    }
-
-
-    /*
-    public Book showBook(@RequestBody String name) {
-        System.out.println("index get  " + name);
-        Book book = bookDao.getBookByName(name);
-        System.out.println(book.getIntroduction());
-        return book;
-    }
-
-     */
-
-
-    /**
-    @CrossOrigin
-    @RequestMapping(value = "/api/index", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+    @RequestMapping(value = "/api/showBook", method = RequestMethod.GET)
     @ResponseBody
     public Book showBook(@RequestBody String name) {
         Book book = bookDao.getBookByName(name);
         System.out.println(book.getIntroduction());
         return book;
     }
-    */
+
 }
